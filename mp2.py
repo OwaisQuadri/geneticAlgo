8b4217f6a7d21566aef007b49266a6b0d9d8f059
Owais Quadri Home PC <owais.quadri@ontariotechu.net>
2022-03-22 21:56:54 -0400

    dev

--- mp2.py
+++ mp2.py
@@ -4,4 +4,163 @@ from typing import List
 
 import argparse 
 
+import time
+
+mutation_prob = 0.05
+#class for board
+class Board:
+    #constructor
+    def __init__(self, board: List[int] = None):
+        self.prob = -1
+        self.board: List[int] = [randint(0,7) for _ in range(8)] if board is None else board.copy()
+        self.fit = self.calcFit()
+        
+    #calculate fitness for board
+    def calcFit(self) -> float:
+        
+        
+        sumFit = 0
+
+        for i in range(7):
+            q1 = self.board[i]
+            currFit = 0
+
+            for j in range(i + 1, 8):
+                d = j - i
+                q2 = self.board[j]
+
+                # check for queens in all directions
+                if q1 == q2 or q1 == q2 + d or q1 == q2 - d:
+                    currFit += 1
+            sumFit += currFit
+
+        return sumFit
+    #create a copy of the board
+    def copy(self) -> 'Board':
+        return Board(self.board)
+    #make a mutation by changing the position of thje queen
+    def mutate(self): 
+
+        if mutation_prob > uniform(0,1):
+            i = randint(0, 7)
+            v = randint(0,7)
+
+            while self.board[i] is v:
+                v = randint(0,7)
+
+            self.board[i] = v
+        
+        # calculate new fitness value
+        self.fit = self.calcFit()
+
+def roulette(brds: List[Board]):
+
+    total_fitness = sum(1 / board.fit for board in brds)
+    offset = 0
+
+    for board in brds:
+        board.prob = offset + 1 / board.fit / total_fitness
+        offset = board.prob
+
+
+def selectParents(boards: List[Board]) -> (Board, Board):
+    #randomly sel;ect parents
+    u1 = uniform(0,1)
+    u2 = uniform(0,1)
+   
+    p1, p2 = None, None
+    for board in boards:
+        if board.prob > u1:
+            p1 = board
+            break
+    
+    for board in boards:
+        if board.prob > u2:
+            p2 = board
+            break
+
+    return p1, p2
+
+def crossover(parent_1: Board, parent_2: Board) -> (Board, Board):
+    #clone/create braeakpoint at random
+
+    c1, c2 = parent_1.copy(), parent_2.copy()
+
+    if 0.3 > uniform(0,1):
+        return c1, c2
+
+    bp = randint(1, 6)
+
+    c1.board[bp:], c2.board[bp:] = c2.board[bp:], c1.board[bp:]
+
+    return c1, c2
+
+def new_population(n: int) -> List[Board]:
+    return [Board() for _ in range(n)]
+
+if __name__ == '__main__':
+
+    x = 30 #population size
+    mutation_prob= .05 # mutation probability
+    num_solutions = 1 # number of solutions wanted
+
+    sols = set()
+    
+    boards = new_population(x)
+    
+    generations = 0
+
+    if num_solutions != 1:
+        print(f"Solutions wanted: {num_solutions}")
+
+    if mutation_prob != 0.01:
+        print(f"Mutation probability: {mutation_prob}")
+
+    print(f"Population size: {x}")
+
+    #start timer
+    start = time.time()
+
+    while True:
+
+        generations += 1
+        
+        sol = False
+        for board in boards:
+            if board.fit == 0:
+                print( generations, "generations later: Solution: ", board.board)
+                sols.add(hash(tuple(board.board)))
+                sol = True
+                break
+
+        # reset if solution
+        if sol:
+            num_solutions = len(sols)
+            if num_solutions == num_solutions:
+                break
+
+            print("Number of solutions:", num_solutions)
+            boards = new_population(x)
+            generations = 0
+
+        roulette(boards)
+
+        generation = []
+
+        for _ in range(int(x/2)):
+            p1, p2 = selectParents(boards)
+            c1, c2 = crossover(p1, p2)
+
+            c1.mutate()
+            c2.mutate()
+
+            generation.append(c1)
+            generation.append(c2)
+
+        boards = generation
+
+    #end timer
+    total_time=time.time()-start
+    print(f"Finished after {total_time} ms")
+
 
